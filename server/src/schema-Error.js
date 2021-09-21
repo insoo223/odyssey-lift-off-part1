53bcc4a3861ae2e413ab19f3a84cf2d4225e2c81
Insoo Kim <insoo@timmanage.com>
2021-09-21 16:12:46 +0900

    schema running normal v. error code

---
+++ server/src/schema-Error.js
@@ -0,0 +1,47 @@
+//npm run start (error)
+// gql: a tagged template literal, used for wrapping GraphQL strings like the schema definition we're about to write.
+// This converts GraphQL strings into the format that Apollo libraries expect when working with operations and schemas, and it also enables syntax highlighting.
+const { gql } = require('apollo-server');
+
+const typeDefs = gql `
+    "how to tell the GraphQL server what to retrieve when we query it. Remember, we don't have multiple specific endpoints to target different types like a REST API does. Instead, we define a special Query type."
+    "The fields of this type are entry points into the rest of our schema. These are the top-level fields that our client can query for."
+    type Query {
+        "Get tracks array for homepage grid"
+        tracksForHome: [Track!]!
+    }
+
+    "A track is a group of Modules that teaches about a specific topic"
+    type Track {
+        id: ID!
+
+        "The track's title"
+        title: String!
+        
+        "The track's main author"
+        author: Author!
+
+        "The track's main illustration to display in track card or track page detail"
+        thumbnail: String
+        
+        "The track's approximate length to complete, in minutes"
+        length: Int
+
+        "The number of modules this track contains"
+        moduleCount: Int
+    }
+
+    "Author of a complete Track or a Module"
+    type Author {
+        id: ID!
+
+        "Author's first and last name"
+        name: String!
+
+        "Author's profile picture url"
+        photo: String
+    }
+
+`;
+
+module.exports = typeDefs;
--- server/src/schema.js
+++ server/src/schema.js
@@ -1,45 +1,34 @@
-// gql: a tagged template literal, used for wrapping GraphQL strings like the schema definition we're about to write.
-// This converts GraphQL strings into the format that Apollo libraries expect when working with operations and schemas, and it also enables syntax highlighting.
-const {gql} = require('apollo-server');
+const { gql } = require('apollo-server');
 
-const typeDefs = gql `
-    "A track is a group of Modules that teaches about a specific topic"
-    type Track {
-        id: ID!
+const typeDefs = gql`
+  type Query {
+    "Query to get tracks array for the homepage grid"
+    tracksForHome: [Track!]!
+  }
 
-        "The track's title"
-        title: String!
-        
-        "The track's main author"
-        author: Author!
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    "The track's title"
+    title: String!
+    "The track's main Author"
+    author: Author!
+    "The track's illustration to display in track card or track page detail"
+    thumbnail: String
+    "The track's approximate length to complete, in minutes"
+    length: Int
+    "The number of modules this track contains"
+    modulesCount: Int
+  }
 
-        "The track's main illustration to display in track card or track page detail"
-        thumbnail: String
-        
-        "The track's approximate length to complete, in minutes"
-        length: Int
-
-        "The number of modules this track contains"
-        moduleCount: Int
-    }
-
-    "Author of a complete Track or a Module"
-    type Author {
-        id: ID!
-
-        "Author's first and last name"
-        name: String!
-
-        "Author's profile picture url"
-        photo: String
-    }
-
-    "how to tell the GraphQL server what to retrieve when we query it. Remember, we don't have multiple specific endpoints to target different types like a REST API does. Instead, we define a special Query type."
-    "The fields of this type are entry points into the rest of our schema. These are the top-level fields that our client can query for."
-    type Query {
-        "Get tracks array for homepage grid"
-        tracksForHome: [Track!]!
-    }
+  "Author of a complete Track or a Module"
+  type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture"
+    photo: String
+  }
 `;
 
 module.exports = typeDefs;
